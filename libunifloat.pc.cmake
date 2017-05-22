6794ab4e79934ec2ec09b62a08e1498696a558b6
Andrey Ponomarenko <andrewponomarenko@yandex.ru>
2017-05-22 08:15:44 +0300

    Improved README.

--- libunifloat.pc.cmake
+++ libunifloat.pc.cmake
@@ -1,6 +1,5 @@
 Name: ${APPLICATION_NAME}
-Description: A C library for high-precision floating-point computations
+Description: A C library for fast high-precision floating-point computations
 Version: ${APPLICATION_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lunifloat
 Cflags: -fno-builtin -I${CMAKE_INSTALL_PREFIX}/include/unifloat-1.0
-
